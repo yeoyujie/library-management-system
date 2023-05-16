72eefde21eb2b755800672164cf941f2757011a5
yeoyujie <yeo.yujie@gmail.com>
2023-05-16 09:50:11 +0800

    Linking formsg to netlify

---
+++ functions/formsg-webhook.js
@@ -0,0 +1,21 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+exports.handler = async (event, context) => {
+  // Get the form response data from the request body
+  const formData = JSON.parse(event.body);
+
+  // TODO: Decrypt the form response data
+
+  // TODO: Process the form response data and save it to your Firebase database
+
+  // Return a response to acknowledge receipt of the webhook event
+  return {
+    statusCode: 200,
+    body: 'Webhook event received',
+  };
+};
