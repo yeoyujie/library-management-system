eda84fd2ecb83af3706e170d0afcb098499efab8
yeoyujie <yeo.yujie@gmail.com>
2023-05-16 12:56:28 +0800

    Parsing JSON data according to formsg repo

--- functions/formsg-webhook/formsg-webhook.js
+++ functions/formsg-webhook/formsg-webhook.js
@@ -1,9 +1,51 @@
-exports.handler = async function(event, context) {
+// Instantiating formsg-sdk without parameters default to using the package's
+// production public signing key.
+const formsg = require("@opengovsg/formsg-sdk")();
+
+// Your form's secret key downloaded from FormSG upon form creation
+const formSecretKey = process.env.FORM_SECRET_KEY;
+
+exports.handler = async function (event, context) {
+  try {
+    formsg.webhooks.authenticate(
+      event.headers["x-formsg-signature"],
+      process.env.URL + "/.netlify/functions/formsg-webhook"
+    );
+  } catch (e) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: "Unauthorized" }),
+    };
+  }
+
   // Parse the data from Formsg
   const data = JSON.parse(event.body);
 
-  // Log the data to the console
-  console.log(data);
+  // Decrypt the data using the secret key
+  const decryptedData = formsg.crypto.decrypt(
+    formSecretKey,
+    data.encryptedContent
+  );
+
+  // Parse the decrypted data as a JSON object
+  const formData = JSON.parse(decryptedData);
+
+  // Access the responses array
+  const responses = decryptedData.responses;
+
+  // Find the response objects for the Book Title and Book Author questions
+  const bookTitleResponse = responses.find(
+    (response) => response.question === "Book Title"
+  );
+  const bookAuthorResponse = responses.find(
+    (response) => response.question === "Book Author"
+  );
+
+  // Access the answer values for the Book Title and Book Author questions
+  const bookTitle = bookTitleResponse.answer;
+  const bookAuthor = bookAuthorResponse.answer;
+
+  console.log(bookTitle, bookAuthor);
 
   // Your code here to handle the book request
 };
