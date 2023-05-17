c2c10f5b346cc291dc8eb6a1cab3628a92fc587d
yeoyujie <yeo.yujie@gmail.com>
2023-05-17 09:26:24 +0800

    Change from Firebase client to firebase admin sdk

--- functions/formsg-webhook/formsg-webhook.js
+++ functions/formsg-webhook/formsg-webhook.js
@@ -1,12 +1,11 @@
-import {
-  getDatabase,
-  ref,
-  query,
-  orderByChild,
-  equalTo,
-  update,
-} from "firebase/database";
-import { app } from "../../src/firebase_setup/firebase.js";
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.database();
 
 // Instantiating formsg-sdk without parameters default to using the package's
 // production public signing key.
@@ -16,23 +15,23 @@ const formsg = require("@opengovsg/formsg-sdk")();
 const formSecretKey = process.env.FORM_SECRET_KEY;
 
 exports.handler = async function (event, context) {
-  // try {
-  //   formsg.webhooks.authenticate(
-  //     event.headers["x-formsg-signature"],
-  //     process.env.URL + "/.netlify/functions/formsg-webhook"
-  //   );
-  // } catch (e) {
-  //   return {
-  //     statusCode: 401,
-  //     body: JSON.stringify({ message: "Unauthorized" }),
-  //   };
-  // }
+  try {
+    formsg.webhooks.authenticate(
+      event.headers["x-formsg-signature"],
+      process.env.URL + "/.netlify/functions/formsg-webhook"
+    );
+  } catch (e) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: "Unauthorized" }),
+    };
+  }
 
   // Parse the data from Formsg
   const data = JSON.parse(event.body);
 
-  console.log("This is the data")
-  console.log(data)
+  console.log("This is the data");
+  console.log(data);
 
   // Decrypt the data using the secret key
   const decryptedData = formsg.crypto.decrypt(
@@ -40,8 +39,8 @@ exports.handler = async function (event, context) {
     data.encryptedContent
   );
 
-  console.log("This is the decrypted data")
-  console.log(decryptedData)
+  console.log("This is the decrypted data");
+  console.log(decryptedData);
 
   // Access the responses array
   const responses = decryptedData.responses;
@@ -60,26 +59,21 @@ exports.handler = async function (event, context) {
 
   console.log(bookTitle, bookAuthor);
 
-    // Query the Firebase database for a book with the specified title and author
-    if (bookTitle && bookAuthor) {
-      try {
-        const db = getDatabase(app);
-        const booksRef = ref(db, 'books');
-        const booksQuery = query(
-          booksRef,
-          orderByChild('title_author'),
-          equalTo(`${bookTitle}_${bookAuthor}`)
-        );
-    
-        // Borrow the first book returned by the query
-        const snapshot = await get(booksQuery);
-        if (snapshot.exists()) {
-          const [bookId] = Object.keys(snapshot.val());
-          await update(ref(db, `books/${bookId}`), { isBorrowed: true });
-          console.log({bookTitle}  + "by " + {bookAuthor} + "is borrowed!")
-        }
-      } catch (error) {
-        console.error(error);
-      }
+  // Query the Firebase database for a book with the specified title and author
+  if (bookTitle && bookAuthor) {
+    try {
+      const booksRef = db.ref("books");
+      booksRef
+        .orderByChild("title_author")
+        .equalTo(`${bookTitle}_${bookAuthor}`)
+        .once("value", (snapshot) => {
+          if (snapshot.exists()) {
+            const [bookId] = Object.keys(snapshot.val());
+            db.ref(`books/${bookId}`).update({ isBorrowed: true });
+          }
+        });
+    } catch (error) {
+      console.error(error);
     }
+  }
 };
