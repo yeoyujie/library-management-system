0e0b64ab5523b57f32f8a624204cd83c356798f3
yeoyujie <yeo.yujie@gmail.com>
2023-05-19 15:59:49 +0800

    Made some UI changes

--- src/components/LayoutForm.js
+++ src/components/LayoutForm.js
@@ -1,22 +1,32 @@
 import React from "react";
 import bgPhoto from "../assets/bgPhoto.jpg";
+import Lottie from "react-lottie";
+import animationData from "../assets/animation.json";
+
 const LayoutForm = ({
   children,
   successMessage,
   errorMessage,
   bookListContent,
 }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div className="container">
-      <div
-        className="search-form"
-        style={{
-          backgroundImage: `url(${bgPhoto})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="search-form-card">{children}</div>
+      <div className="search-form" style={{ position: "relative" }}>
+        <Lottie
+          options={defaultOptions}
+          style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+        />
+        <div className="search-form-card" style={{ zIndex: 1 }}>
+          {children}
+        </div>
       </div>
       <div className="search-results">
         {successMessage && (
